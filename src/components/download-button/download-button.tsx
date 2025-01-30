0323b92d7d8eaafa1f0fe5f5a7877b5034532041
Tiger Abrodi <tigerabrodi@gmail.com>
2025-01-30 19:53:23 +0100

    download button

--- src/components/download-button/download-button.tsx
+++ src/components/download-button/download-button.tsx
@@ -1,3 +1,28 @@
-export function DownloadButton() {
-  return <button>Download</button>
+import './styles.css'
+
+export default function DownloadButton() {
+  return (
+    <button aria-label="Download PDF" className="download-button">
+      {ArrowDown}
+      {ArrowDown}
+    </button>
+  )
 }
+
+const ArrowDown = (
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18.25 14L12 20.25L5.75 14M12 19.5V3.75"
+      stroke="black"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)

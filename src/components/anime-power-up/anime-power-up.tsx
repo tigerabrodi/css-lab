1eed08071c9b88bdfa4e4e6e050a29a082156d6f
Tiger Abrodi <tigerabrodi@gmail.com>
2025-01-31 16:58:59 +0100

    anime powerup

---
+++ src/components/anime-power-up/anime-power-up.tsx
@@ -0,0 +1,10 @@
+import naruto from './naruto.png'
+import './styles.css'
+
+export function AnimePowerUp() {
+  return (
+    <div className="power-up">
+      <img src={naruto} alt="Naruto" />
+    </div>
+  )
+}

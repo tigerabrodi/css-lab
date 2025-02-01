da5975044d255bcb1b6e55605b1c98a62b587068
Tiger Abrodi <tigerabrodi@gmail.com>
2025-02-01 08:07:22 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import { ChakraLoader } from './components/chakra-loader/chakra-loader'
 import { Coin } from './components/coin/coin'
 import { DownloadButton } from './components/download-button/download-button'
 import { EnergyFlow } from './components/energy-flow/energy-flow'
+import { ImageGridHover } from './components/image-grid-hover/image-grid-hover'
 import { Orbit } from './components/orbit/orbit'
+import { RetroLoading } from './components/retro-loading/retro-loading'
+import { SlideUpCards } from './components/slide-up-cards/slide-up-cards'
 import { SonnerToast } from './components/sonner-toast/sonnet-toast'
+import { StaggeredGridItem } from './components/staggered-grid-item/staggered-grid-item'
+import { TerminalTypingEffect } from './components/terminal-typing-effect/terminal-typing-effect'
 import { TextReveal } from './components/text-reveal/text-reveal'
 import { ComponentLayout } from './layouts/component'
 import { ROUTES } from './lib/constants'
@@ -38,6 +43,31 @@ function App() {
         />
 
         <Route path={ROUTES.ownCreations.energyFlow} element={<EnergyFlow />} />
+
+        <Route
+          path={ROUTES.ownCreations.retroLoading}
+          element={<RetroLoading />}
+        />
+
+        <Route
+          path={ROUTES.ownCreations.imageGridHover}
+          element={<ImageGridHover />}
+        />
+
+        <Route
+          path={ROUTES.ownCreations.staggeredGridItem}
+          element={<StaggeredGridItem />}
+        />
+
+        <Route
+          path={ROUTES.ownCreations.slideUpCards}
+          element={<SlideUpCards />}
+        />
+
+        <Route
+          path={ROUTES.ownCreations.terminalTypingEffect}
+          element={<TerminalTypingEffect />}
+        />
       </Route>
     </Routes>
   )

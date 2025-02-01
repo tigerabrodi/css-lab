1cf4f395695a34cb5932b53699681062f5a0b104
Tiger Abrodi <tigerabrodi@gmail.com>
2025-02-01 08:06:00 +0100

    retro loading

---
+++ src/components/retro-loading/retro-loading.tsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export function RetroLoading() {
+  return (
+    <div className="loading-container">
+      <div className="loading-text">LOADING...</div>
+      <div className="loading-bar"></div>
+    </div>
+  )
+}
